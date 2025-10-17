488064ffde1f75dc84760598cb09f62ebc093fa8
Ana Helena <anahelenarp@hotmail.com>
2025-10-17 16:29:27 -0300

    feat: auth screen and components

---
+++ src/view/components/Button.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps } from "react";
+
+interface ButtonProps extends ComponentProps<'button'> {
+  name?: string;
+}
+
+export function Button(props: ButtonProps) {
+
+  return (
+    <button
+      {...props}
+      className="bg-teal-900 hover:bg-teal-800 px-6 h-12 rounded-2xl text-white font-medium disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed transition-all"
+    />
+  );
+}
