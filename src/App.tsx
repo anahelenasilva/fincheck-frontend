fe29a28d14c2f9e6b089d737b26501e52809f4fe
Ana Helena <anahelenarp@hotmail.com>
2025-10-19 09:43:14 -0300

    chore: add tanstack react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import { Router } from "./Router";
@@ -21,6 +22,8 @@ export function App() {
 
         <Toaster />
       </AuthProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
