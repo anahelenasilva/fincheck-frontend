72f667edc879b17eb709dc8d101c461f49eb192e
Ana Helena <anahelenarp@hotmail.com>
2025-10-20 20:32:28 -0300

    feat: transactions list details

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -2,6 +2,8 @@ import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { MONTHS } from "../../../../../app/config/constants";
+import { formatCurrency } from "../../../../../app/shared/number/formatCurrency";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
 import { FilterIcon } from "../../../../components/icons/FilterIcon";
 import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
 import { SliderNavigation } from "./SliderNavigation";
@@ -9,8 +11,8 @@ import { SliderOption } from "./SliderOption";
 
 export function Transactions() {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full md:p-10 px-4 py-8">
-      <header className="">
+    <div className="bg-gray-100 rounded-2xl w-full h-full md:p-10 px-4 py-8 flex flex-col">
+      <header>
         <div className="flex items-center justify-between">
           <button className="flex items-center gap-2">
             <TransactionsIcon />
@@ -45,8 +47,36 @@ export function Transactions() {
         </div>
       </header>
 
-      <div className="mt-4">
-        Conteúdo
+      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="expense" />
+
+            <div className="">
+              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+              <span className="text-sm text-gray-600">04/06/2025</span>
+            </div>
+          </div>
+
+          <span className="font-medium tracking-[-0.5px] text-red-800">
+            -{formatCurrency(123)}
+          </span>
+        </div>
+
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="income" />
+
+            <div className="">
+              <strong className="font-bold tracking-[-0.5px] block">Salário</strong>
+              <span className="text-sm text-gray-600">01/06/2025</span>
+            </div>
+          </div>
+
+          <span className="font-medium tracking-[-0.5px] text-green-800">
+            {formatCurrency(8000)}
+          </span>
+        </div>
       </div>
     </div>
   );
