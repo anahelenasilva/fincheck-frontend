70efa4538465623f0ceb0f6ad2894abbe4645528
Ana Helena <anahelenarp@hotmail.com>
2025-10-20 21:00:13 -0300

    feat: accounts and transactions loading

--- src/view/pages/Dashboard/components/Accounts/index.tsx
+++ src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { formatCurrency } from '../../../../../app/shared/number/formatCurrency';
 import { mergeClassNames } from '../../../../../app/shared/strings/mergeClassNames';
 import { EyeIcon } from "../../../../components/icons/EyeIcon";
+import { Spinner } from '../../../../components/Spinner';
 import { AccountCard } from './AccountCard';
 import { SliderNavigation } from './SliderNavigation';
 import { useAccountsController } from './useAccountsController';
@@ -16,64 +17,75 @@ export function Accounts() {
     windowWidth,
     areValuesVisible,
     toggleValuesVisibility,
+    isLoading,
   } = useAccountsController();
 
   return (
     <div className="bg-teal-900 rounded-2xl w-full h-full md:p-10 px-4 py-8 flex flex-col">
-      <div className="">
-        <span className="tracking-[-0.5px] text-white block">Saldo Total</span>
-
-        <div className="flex items-center gap-2">
-          <strong
-            className={mergeClassNames("text-2xl tracking-[-1px] text-white", !areValuesVisible && 'blur-md')}>
-            {formatCurrency(1000)}
-          </strong>
-
-          <button
-            className="w-8 h-8 flex items-center justify-center"
-            onClick={toggleValuesVisibility}
-          >
-            <EyeIcon open={!areValuesVisible} />
-          </button>
+      {isLoading && (
+        <div className='w-full h-full flex items-center justify-center'>
+          <Spinner className='text-teal-950/50 fill-white w-10 h-10' />
         </div>
-      </div>
+      )}
 
-      <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
-        <div>
-          <Swiper
-            spaceBetween={16}
-            slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
-            onSlideChange={swiper => {
-              setSliderState({
-                isBeginning: swiper.isBeginning,
-                isEnd: swiper.isEnd,
-              });
-            }}
-          >
-            <div className="flex items-center justify-between mb-4" slot="container-start">
-              <strong className="text-white tracking-[-1px] text-lg font-bold">
-                Minhas contas
+      {!isLoading && (
+        <>
+          <div>
+            <span className="tracking-[-0.5px] text-white block">Saldo Total</span>
+
+            <div className="flex items-center gap-2">
+              <strong
+                className={mergeClassNames("text-2xl tracking-[-1px] text-white", !areValuesVisible && 'blur-md')}>
+                {formatCurrency(1000)}
               </strong>
 
-              <SliderNavigation
-                isBeginning={sliderState.isBeginning}
-                isEnd={sliderState.isEnd} />
+              <button
+                className="w-8 h-8 flex items-center justify-center"
+                onClick={toggleValuesVisibility}
+              >
+                <EyeIcon open={!areValuesVisible} />
+              </button>
             </div>
+          </div>
 
-            <SwiperSlide>
-              <AccountCard color="#ff0" name="Itaú" balance={1000.23} type="CASH" />
-            </SwiperSlide>
+          <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
+            <div>
+              <Swiper
+                spaceBetween={16}
+                slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
+                onSlideChange={swiper => {
+                  setSliderState({
+                    isBeginning: swiper.isBeginning,
+                    isEnd: swiper.isEnd,
+                  });
+                }}
+              >
+                <div className="flex items-center justify-between mb-4" slot="container-start">
+                  <strong className="text-white tracking-[-1px] text-lg font-bold">
+                    Minhas contas
+                  </strong>
 
-            <SwiperSlide>
-              <AccountCard color="#adf" name="XP" balance={25000} type="INVESTMENT" />
-            </SwiperSlide>
+                  <SliderNavigation
+                    isBeginning={sliderState.isBeginning}
+                    isEnd={sliderState.isEnd} />
+                </div>
 
-            <SwiperSlide>
-              <AccountCard color="#bae" name="Carteira" balance={25} type="CASH" />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
+                <SwiperSlide>
+                  <AccountCard color="#ff0" name="Itaú" balance={1000.23} type="CASH" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <AccountCard color="#adf" name="XP" balance={25000} type="INVESTMENT" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <AccountCard color="#bae" name="Carteira" balance={25} type="CASH" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -7,81 +7,92 @@ import { mergeClassNames } from "../../../../../app/shared/strings/mergeClassNam
 import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
 import { FilterIcon } from "../../../../components/icons/FilterIcon";
 import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
+import { Spinner } from "../../../../components/Spinner";
 import { SliderNavigation } from "./SliderNavigation";
 import { SliderOption } from "./SliderOption";
 import { useTransactionsController } from "./useTransactionsController";
 
 export function Transactions() {
-  const { areValuesVisible } = useTransactionsController();
+  const { areValuesVisible, isLoading } = useTransactionsController();
 
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full md:p-10 px-4 py-8 flex flex-col">
-      <header>
-        <div className="flex items-center justify-between">
-          <button className="flex items-center gap-2">
-            <TransactionsIcon />
-            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
-            <ChevronDownIcon className="text-gray-900" />
-          </button>
-
-          <button>
-            <FilterIcon />
-          </button>
+      {isLoading && (
+        <div className='w-full h-full flex items-center justify-center'>
+          <Spinner className='w-10 h-10' />
         </div>
+      )}
 
-        <div className="mt-6 relative">
-          <Swiper
-            slidesPerView={3}
-            centeredSlides
-          >
-            <SliderNavigation />
+      {!isLoading && (
+        <>
+          <header>
+            <div className="flex items-center justify-between">
+              <button className="flex items-center gap-2">
+                <TransactionsIcon />
+                <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
+                <ChevronDownIcon className="text-gray-900" />
+              </button>
 
-            {MONTHS.map((month, index) => (
-              <SwiperSlide key={month}>
-                {({ isActive }) => (
-                  <SliderOption
-                    month={month}
-                    isActive={isActive}
-                    index={index}
-                  />
-                )}
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </header>
+              <button>
+                <FilterIcon />
+              </button>
+            </div>
 
-      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-          <div className="flex-1 flex items-center gap-3">
-            <CategoryIcon type="expense" />
+            <div className="mt-6 relative">
+              <Swiper
+                slidesPerView={3}
+                centeredSlides
+              >
+                <SliderNavigation />
 
-            <div className="">
-              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
-              <span className="text-sm text-gray-600">04/06/2025</span>
+                {MONTHS.map((month, index) => (
+                  <SwiperSlide key={month}>
+                    {({ isActive }) => (
+                      <SliderOption
+                        month={month}
+                        isActive={isActive}
+                        index={index}
+                      />
+                    )}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </header>
+
+          <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+              <div className="flex-1 flex items-center gap-3">
+                <CategoryIcon type="expense" />
+
+                <div className="">
+                  <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+                  <span className="text-sm text-gray-600">04/06/2025</span>
+                </div>
+              </div>
+
+              <span className={mergeClassNames("font-medium tracking-[-0.5px] text-red-800", !areValuesVisible && 'blur-sm')}>
+                -{formatCurrency(123)}
+              </span>
+            </div>
+
+            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+              <div className="flex-1 flex items-center gap-3">
+                <CategoryIcon type="income" />
+
+                <div className="">
+                  <strong className="font-bold tracking-[-0.5px] block">Salário</strong>
+                  <span className="text-sm text-gray-600">01/06/2025</span>
+                </div>
+              </div>
+
+              <span className={mergeClassNames("font-medium tracking-[-0.5px] text-green-800", !areValuesVisible && 'blur-sm')}>
+                {formatCurrency(8000)}
+              </span>
             </div>
           </div>
-
-          <span className={mergeClassNames("font-medium tracking-[-0.5px] text-red-800", !areValuesVisible && 'blur-sm')}>
-            -{formatCurrency(123)}
-          </span>
-        </div>
-
-        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-          <div className="flex-1 flex items-center gap-3">
-            <CategoryIcon type="income" />
-
-            <div className="">
-              <strong className="font-bold tracking-[-0.5px] block">Salário</strong>
-              <span className="text-sm text-gray-600">01/06/2025</span>
-            </div>
-          </div>
-
-          <span className={mergeClassNames("font-medium tracking-[-0.5px] text-green-800", !areValuesVisible && 'blur-sm')}>
-            {formatCurrency(8000)}
-          </span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
